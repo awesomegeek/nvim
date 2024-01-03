ea05b6bae266a8ad4f438eda6bfd844d61a022f7
Naing Tun Win <naingtunwin@gmail.com>
2024-01-03 15:33:41 +0800

    Add popup for prayer times

--- prayertime.nvim/lua/prayertime/init.lua
+++ prayertime.nvim/lua/prayertime/init.lua
@@ -1,5 +1,47 @@
 local M = {}
 
+local popup = require "plenary.popup"
+
+local Win_id
+
+function CloseMenu()
+   vim.api.nvim_win_close(Win_id, true)
+ end
+
+function ShowMenu(opts, cb)
+  local height = 20
+  local width = 30
+  local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+
+  Win_id = popup.create(opts, {
+        title = "Prayer Times",
+        highlight = "Prayer Times for Cyberjaya",
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minwidth = width,
+        minheight = height,
+        borderchars = borderchars,
+        callback = cb,
+  })
+  local bufnr = vim.api.nvim_win_get_buf(Win_id)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<cmd>lua CloseMenu()<CR>", { silent=false })
+end
+
+function MyMenu()
+   local opts = {
+      "Fajir: 05:45",
+      "Sunrise: 07:10",
+      "Dhuhr: 13:00",
+      "Asr: 16:30",
+      "Maghrib: 19:15",
+      "Isha: 20:30",
+      "Midnight: 00:00",
+   }
+   local cb = function(_, sel)
+     print("it works")
+   end
+   ShowMenu(opts, cb)
+ end
 
 function M.get_prayer_times(opts)
     opts = opts or {}
@@ -20,7 +62,8 @@ function M.get_prayer_times(opts)
    local prayTime = require('prayertime.ptime'):new();
    prayTime:setCalcMethod(prayTime.MWL);
    local times = prayTime:getPrayerTimes(os.date("*t"), coords[1], coords[2]);
-   print('Fajr' , times[1]);
+   -- print('Fajr' , times[1]);
+   MyMenu()
 end
 
 function M.setup(opts)
